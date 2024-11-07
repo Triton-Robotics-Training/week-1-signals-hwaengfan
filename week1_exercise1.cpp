4f75f385324a53816a9d9732e1c0a029cf58e6e0
hwaengfan <21hoangphan@gmail.com>
2024-11-07 13:13:02 -0800

    week 1 assignment

--- week1_exercise1.cpp
+++ week1_exercise1.cpp
@@ -1,14 +1,11 @@
-//STARTER 
-//THIS EXERCISE IS TO BE DONE IN THE ARM MBED SIMULATOR
-//http://195.130.59.221/
-//IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
+
+DigitalIn button(BUTTON1);
+DigitalOut led(LED1);
 
 int main() {
     while (1) {
-        printf("Week 1 Exercise 1");
-
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
+        led = button.read();
         wait_ms(500); 
     }
 }
--- week1_exercise2.cpp
+++ week1_exercise2.cpp
@@ -1,14 +1,17 @@
-//STARTER 
-//THIS EXERCISE IS TO BE DONE IN THE ARM MBED SIMULATOR
-//http://195.130.59.221/
-//IF YOU HAVE ISSUES WITH THE SIMULATOR, CONTACT EMBEDDED LEAD
 #include "mbed.h"
 
-int main() {
-    while (1) {
-        printf("Week 1 Exercise 2");
+DigitalOut led(LED1);
+AnalogIn analogInput(p15);
 
-        // MAKE SURE THERE IS ALWAYS A WAIT ON THE SIM OR IT WILL CRASH
-        wait_ms(500); 
+int main() {
+    double period = 2000;
+    
+    while (1) {
+        double percentage = analogInput.read();
+        
+        led = !led;
+        wait_ms(period * percentage);
+        led = !led;
+        wait_ms(period * (1 - percentage));
     }
 }
